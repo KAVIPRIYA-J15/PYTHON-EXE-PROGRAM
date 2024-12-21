7ba6290e3bbba44637f2576cb81d4ffca4dcf687
KAVIPRIYA-J15 <23BCS045@PSGRKCW.AC.IN>
2024-12-21 13:42:58 +0530

    Add files via upload

---
+++ p11.py
@@ -0,0 +1,5 @@
+a=int(input("enter the principal amounnt:"))
+b=int(input("enter the number of years:"))
+c=int(input("enter the rate of interest:"))
+SI=a*b*c/100
+print("the simple interest is:",SI)
